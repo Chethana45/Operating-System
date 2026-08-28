3ce7b663cfae5dbc1063af76d4bc70a8d42ae254
Chethana45 <chethanasriadusumalli@gmail.com>
2026-08-28 20:21:03 +0530

    Create prime.sh

---
+++ Shellprogramming/prime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read n
+
+if [ $n -le 1 ]; then
+    echo "Not Prime"
+    exit
+fi
+
+flag=0
+
+for ((i=2; i<n; i++))
+do
+    if [ $((n % i)) -eq 0 ]; then
+        flag=1
+        break
+    fi
+done
+
+if [ $flag -eq 0 ]; then
+    echo "Prime"
+else
+    echo "Not Prime"
+fi
